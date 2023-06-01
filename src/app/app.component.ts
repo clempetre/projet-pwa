a9ddbd9d4f28ace6ea21d5cf6dc365f70d756eca
clempetre <71828831+clempetre@users.noreply.github.com>
2023-06-01 11:39:17 +0200

    mag icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
+import { NewsletterService } from './services/newsletter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
   title = 'projet-pwa';
+  readonly VAPID_PUBLIC_KEY = "BDuF8VgyRArMT9ZxvRnyWZMVHVPgMDPI5UA0lrixak-JVyIHZWoB5HePS_Rhrs2nGrG84snWHnnYq0vS4wnVhT8";
+
+  constructor(
+      private swPush: SwPush,
+      private newsletterService: NewsletterService) {}
+
+  subscribeToNotifications() {
+
+      this.swPush.requestSubscription({
+          serverPublicKey: this.VAPID_PUBLIC_KEY
+      })
+      .then(sub => this.newsletterService.addPushSubscriber(sub).subscribe())
+      .catch(err => console.error("Could not subscribe to notifications", err));
+  }
 }
