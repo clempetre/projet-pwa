e30e966fa598209ea352c60bed3c3983cfefec4c
clempetre <71828831+clempetre@users.noreply.github.com>
2023-06-21 11:26:51 +0200

    add ios bundle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,32 @@ import { NewsletterService } from './services/newsletter.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-
 })
 export class AppComponent {
   title = 'projet-pwa';
   readonly VAPID_PUBLIC_KEY = "BDuF8VgyRArMT9ZxvRnyWZMVHVPgMDPI5UA0lrixak-JVyIHZWoB5HePS_Rhrs2nGrG84snWHnnYq0vS4wnVhT8";
 
   constructor(
-      private swPush: SwPush,
-      private newsletterService: NewsletterService) {}
+    private swPush: SwPush,
+    private newsletterService: NewsletterService
+  ) {}
 
   subscribeToNotifications() {
-
-      this.swPush.requestSubscription({
-          serverPublicKey: this.VAPID_PUBLIC_KEY
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY
       })
-      .then(sub => this.newsletterService.addPushSubscriber(sub).subscribe())
-      .catch(err => console.error("Could not subscribe to notifications", err));
+      .then(sub => {
+        this.newsletterService
+          .addPushSubscriber(sub)
+          .subscribe(
+            () => console.log('Abonnement aux notifications réussi.'),
+            error =>
+              console.error('Erreur lors de l\'abonnement aux notifications', error)
+          );
+      })
+      .catch(err =>
+        console.error('Impossible de s\'abonner aux notifications', err)
+      );
   }
 }
