ac31d0df66e805a2075943781f9d30c93db4fbdf
Alloula Elias <72727849+flicker71@users.noreply.github.com>
2023-06-22 12:27:04 +0200

    qdd routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CameraComponent } from './camera/camera.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { UserComponent } from './component/user/user.component';
+import { ToastComponent } from './toast/toast.component';
 
 const routes: Routes = [
     { path: 'camera', component: CameraComponent },
     { path: 'accueil', component: AccueilComponent },
-    { path: 'user', component: UserComponent },
+    { path: 'user', component: UserComponent },      
+    { path: 'toast', component: ToastComponent },
+
 ];
 
 @NgModule({
